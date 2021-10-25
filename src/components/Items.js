70a07b4606cbfbdfddd749b432c708f8c10656c4
Wilson Tharp <wdtharp@ou.edu>
2021-10-25 07:55:13 -0500

    working on add to cart button, do not merge with main

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,131 +1,13 @@
-// import {React, useState, useEffect} from 'react';
-// import {handleItems, handleCreateCart} from '../API/index';
-// import {useHistory, Link} from 'react-router-dom'
-
-// //ASK SHANNON, how to have multiple counters on the same page? They all update together
-// //somehow use the item id to change count? Very confused. 
-// //clickerInUse is null. not sure why.
-
-
-// const Clicker = ({
-// 	addCount,
-// 	subtractCount,
-// 	id
-// }) => {
-
-// //not sure how to format the onClicks, event listener function?
-// 	return (
-// 		<>
-// 		<section className="ClickerButtons">
-// 		<button onClick={subtractCount}>-</button>
-// 		<button onClick={addCount}>+</button>
-// 		</section>
-// 		</>
-// 	)
-// }
-
-// const Counter = ({
-// 	count
-// }) => {
-// 	return (
-// 		<section className="count">
-// 		<p>Quantity {count}</p>
-// 		</section>
-// 	)
-// }
-
-
-
-// const Items = () => {
-// 	const [count, setCount] = useState(0);
-// 	const [itemId, setItemId] = useState();
-// 	const [userId, setUserId] = useState();
-// 	const [items, setItems] = useState([]);
-
-// 	const addCount = () => {
-		
-// 		setCount(count +1);
-// 	}
-
-// 	const subtractCount = () => {
-		
-// 		{count === 0 ? setCount(0) : setCount(count -1) }
-// 	}
-
-// 	// $('.clickerButtons').click(function(e){
-// 	// 	const newNumber= $(e.target).
-	   
-// 	// 	$('.count').val()
-// 	// })
-
-// 	useEffect(() => {
-// 		try {
-// 			Promise.all([handleItems()]).then(([data]) => {
-// 				setItems(data);
-// 				console.log(data);
-// 			}), [];
-// 		} catch (error) {
-// 			console.log("ERROR", error);
-// 		}
-// 	}, []);
-
-	
-// //need to work on the onclick for the Add to cart button
-// 	return (
-// 		<div>
-			
-// 		<div className="pageContainer">
-// 		<h1 className="pageTitle">Items:</h1>
-// 		<div>
-// 			{items.map((item, id) => {
-// 				console.log(item)
-// 				return (
-// 					<>
-// 					<div className="singleItem">
-// 						<h2>{item.name}</h2>
-// 						<h3>${item.price}</h3>
-// 						<p>{item.description}</p>
-// 						<img src={require(`../images/${item.picture}`).default}></img>
-// 						<div className="quantitySelector">
-// 							<Clicker id = {id}
-// 									addCount = {addCount}
-// 									 subtractCount = {subtractCount}	
-// 							/>
-// 							<Counter count={count}
-// 							/>
-// 						</div>
-						
-// 						<button onClick={handleCreateCart(localStorage.getItem(`username`), item.id, count )}>
-							
-// 						Add to Cart</button>
-						
-// 						<hr />
-// 					</div>
-
-				
-// 					</>
-// 				)
-// 			})}
-// 		</div>
-// 		</div>
-// 	</div>
-// )
-// }
-
-// export default Items;
-
 import { React, useState, useEffect } from "react";
 import { handleItems, handleCreateCart } from "../API/index";
 import { useHistory, Link } from "react-router-dom";
-//ASK SHANNON, how to have multiple counters on the same page? They all update together
-//somehow use the item id to change count? Very confused.
-//clickerInUse is null. not sure why.
-const IndividualItem = ({ item }) => {
+import API from '../API/api';
+
+const IndividualItem = ({ item, userId }) => {
   //moved the count state item into the Clicker since we dont need to access it anywhere inside Items -> only relevant to the clicker
   const [count, setCount] = useState(0);
-  const [itemId, setItemId] = useState();
-  const [userId, setUserId] = useState();
-  const [items, setItems] = useState([]);
+  const [itemId, setItemId] = useState(0);
+  
   //moved both functions inside the Clicker
   const addCount = () => {
     setCount(count + 1);
@@ -134,7 +16,14 @@ const IndividualItem = ({ item }) => {
     {
       count === 0 ? setCount(0) : setCount(count - 1);
     }
+	
+	
   };
+//   console.log('itemid is', itemId)
+  
+
+
+
   return (
     <>
       <div className={"singleItem" + item.id}>
@@ -152,9 +41,11 @@ const IndividualItem = ({ item }) => {
           </button>
         </div>
         <button
-          onClick={() =>
-            handleCreateCart(localStorage.getItem(`username`), item.id, count)
-          }
+		//takes two clicks to setItemId? But why?
+          onClick={() => {
+			setItemId(item.id);
+            handleCreateCart(userId, itemId, count)
+          }}
         >
           Add to Cart
         </button>
@@ -165,11 +56,27 @@ const IndividualItem = ({ item }) => {
 };
 const Items = () => {
   const [items, setItems] = useState([]);
+  const [userId, setUserId] = useState(0);
+
+  useEffect( async function() {
+	try {
+		const username = localStorage.getItem('username');
+			const userData = await API.makeRequest(`/users/${username}`, 'GET');
+			console.log("USERDATA",userData);
+			setUserId(userData.id);
+				
+		
+		} catch (error) {
+			throw error;
+		} 
+	}, []);
+
+console.log('userId is', userId)
+
   useEffect(() => {
     try {
       Promise.all([handleItems()]).then(([data]) => {
         setItems(data);
-        console.log("ITEM ", items);
       }),
         [];
     } catch (error) {
@@ -185,7 +92,8 @@ const Items = () => {
             console.log(item);
             return (
               <div key={item.id}>
-                <IndividualItem item={item} />
+                <IndividualItem item={item}
+				userId= {userId} />
               </div>
             );
           })}
